a0248b7c730706c7eced08d7cbd37990d77af6c6
Sethuraman Bose <sethusharp@gmail.com>
2014-07-30 17:40:11 -0400

    router with menu implementation

--- js/app.js
+++ js/app.js
@@ -1,5 +1,33 @@
-'use strict';
+var myApp = angular.module('myApp',['ngRoute'])
 
+.controller('NavigationCtrl', ['$scope', function($scope) {
+	
+	$scope.menus = [{name:'User', template:'/user'},
+	                  {name:'Account', template:'/account'},
+	                  {name:'Loan', template:'/loan'},
+	                  {name:'Logout', template:'/logout'},
+	                ];
+}])
 
-// Declare app level module which depends on filters, and services
-angular.module('myApp', []);
+.controller('UserController', ['$scope', function($scope) {
+	$scope.firstName ="sethu";
+	$scope.lastName ="bose";
+}])
+
+.controller('AccountController', ['$scope', function($scope) {
+	$scope.accountName ="HDFC";
+	$scope.accountId ="01249873471233";
+}])
+
+.config(function($routeProvider, $locationProvider) {
+	  $routeProvider
+	   .when('/user', {
+	    templateUrl: 'views/user.html',
+	    controller: 'UserController'
+	  })
+	  .when('/account', {
+	    templateUrl: 'views/account.html',
+	    controller: 'AccountController'
+	  });
+	  $locationProvider.html5Mode(true);
+});
